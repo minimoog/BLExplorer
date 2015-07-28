d1dc6930321552de490e022c90674ca4dcc40a41
Toni Jovanoski <minimoog77@gmail.com>
2015-07-28 20:55:20 +0200

    Initial Commit

---
+++ BLExplorer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BLExplorer
+//
+//  Created by Toni Jovanoski on 7/28/15.
+//  Copyright © 2015 Antonie Jovanoski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
