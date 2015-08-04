bd8fa94e987556d42f728e262ffb326298f95b85
Toni Jovanoski <minimoog77@gmail.com>
2015-08-04 21:06:22 +0200

    Added delegates.

--- BLExplorer/ViewController.swift
+++ BLExplorer/ViewController.swift
@@ -7,12 +7,21 @@
 //
 
 import UIKit
+import CoreBluetooth
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CBCentralManagerDelegate {
+    
+    var cbManager: CBCentralManager?
 
+    @IBAction func scanButton() {
+        cbManager?.scanForPeripheralsWithServices(nil, options: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        cbManager = CBCentralManager(delegate: self, queue: nil)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +29,35 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func centralManager(central: CBCentralManager, didConnectPeripheral peripheral: CBPeripheral) {
+        // ### TODO
+    }
+    
+    func centralManager(central: CBCentralManager, didDisconnectPeripheral peripheral: CBPeripheral, error: NSError?) {
+        // ### TODO
+    }
+    
+    func centralManager(central: CBCentralManager, didFailToConnectPeripheral peripheral: CBPeripheral, error: NSError?) {
+        // ### TODO
+    }
+    
+    func centralManager(central: CBCentralManager, didDiscoverPeripheral peripheral: CBPeripheral, advertisementData: [String : AnyObject], RSSI: NSNumber) {
+        
+        print("Discovered \(peripheral.name)")
+    }
+    
+    func centralManagerDidUpdateState(central: CBCentralManager) {
+        if central.state == .PoweredOff {
+            print("Core BLE powered off")
+        } else if central.state == .PoweredOn {
+            print("Core BLE powered on")
+        } else if (central.state == .Unauthorized) {
+            print("Core BLE unauthorized")
+        } else if (central.state == .Unknown) {
+            print("Core BLE state unknown")
+        } else if (central.state == .Unsupported) {
+            print("Core BLE state unsuppored")
+        }
+    }
 }
 
